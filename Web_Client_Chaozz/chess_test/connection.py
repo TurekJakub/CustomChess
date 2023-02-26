521afa380ed6eb383035031fb4b04fd7d5f63fe9
TurekJakub <jakub.turek@student.gyarab.cz>
2023-02-26 23:34:37 +0100

    26/2/23 - ajax calls rework, better chessboard resizing, basic implementation of websockets

--- Web_Client_Chaozz/chess_test/connection.py
+++ Web_Client_Chaozz/chess_test/connection.py
@@ -1,56 +1,63 @@
 import threading
-import socket
+import socket as s
 import ssl
 import select
+from . import routing
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 class Connection:
-  
-    def __init__(self):
-        self.connection = self.establish_connectio()
-        self.message = ""
 
-    def establish_connectio(self):
+    def __init__(self):
+        self.connection = self.establish_connectio('127.0.0.1', 443, 'UwU')
+        self.message = ""
+        self.waiting = False
+
+    def establish_connectio(self, host, port, host_name):
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         context.verify_mode = ssl.CERT_REQUIRED
         context.load_verify_locations('C:/users/jakub/desktop/c.pem')
+        socket = s.create_connection((host, port))
+        return context.wrap_socket(socket, server_hostname=host_name)
 
-        with socket.create_connection(('127.0.0.1', 443)) as socke:
-            with context.wrap_socket(socke, server_hostname='UwU') as c:
-                 self.connection = c
+    def wait_until_next_turn(self):
 
-    def comunnictae(self):
-     end = False
-     while not end:
+        while True:
             message = self.connection.recv().decode('utf-8')
-            end = 'end' in message
-            if ('file' in message):
-                self.receive_files(int(message.split(':')[1]))
-     print('Successfully finished')
+            if ('notify' in message):
+                break
+            if ('update' in message):
+                print('update')
+        print('Successfully finished')
+        layer = get_channel_layer()
+       
 
     def receive_files(self, number_of_files):
-        i = number_of_files
         while number_of_files > 0:
             message = self.connection.recv().decode('utf-8')
-            x = message.split(':')
-            with open('./chess_test/static/temp/' + x[0], 'wb') as file:
-                size = int(x[1])
-                while 0 < size:
+            file_info = message.split(':')
+            self.recieve_file(file_info[0], int(file_info[1]))
+            print('Successfully transferred')
+            number_of_files = number_of_files - 1
 
-                    file_bytes = self.connection.recv(4096)
-                    print(file_bytes)
-                    file.write(file_bytes)
-                    file_bytes = []
-                    size = size - 4096
-                print('Successfully transferred')
-                i = i-1
+    def recieve_file(self, name, size):
+        with open('./chess_test/static/temp/' + name, 'wb') as file:
+            while 0 < size:
+                file_bytes = self.connection.recv(4096)
+                file.write(file_bytes)
+                file_bytes = []
+                size = size - 4096
 
-    def send_figures_move(self, move):
-        self.connection.send(len(move).to_bytes(4, 'big'))
-        self.connection.send(move.encode())
+    def recieve_data(self):
+        return self.connection.recv().decode('utf-8')
 
-    def start_communicatio(self):
-        t = threading.Thread(target=self.comunnicate)
+    def send_data(self, data):
+        self.connection.send(len(data).to_bytes(4, 'big'))
+        self.connection.send(data.encode())
+
+    def start_waiting_until_next_turn(self):
+        t = threading.Thread(target=self.wait_until_next_turn())
         t.setDaemon(True)
         t.start()
 
