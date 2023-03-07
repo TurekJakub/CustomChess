c2c0cf7876869357f6718998d77f0f107332f6fe
TurekJakub <jakub.turek@student.gyarab.cz>
2023-03-07 22:07:42 +0100

    7/3/23 - basic authentication, improved UI

--- Web_Client_Chaozz/chess_test/models.py
+++ Web_Client_Chaozz/chess_test/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Car(models.Model):
+    _id = models.CharField(max_length=100)
+    speed = models.IntegerField(default=1)
+    class Meta:
+        app_label = ''
+        db_table = 'cars'
+
 # Create your models here.
--- Web_Client_Chaozz/chess_test/views.py
+++ Web_Client_Chaozz/chess_test/views.py
@@ -3,10 +3,14 @@ from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .connection import *
+from .models import *
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from .forms import sign_up_form
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
-#c = Connection()
+# c = Connection()
 x = 0
 
 
@@ -25,6 +29,34 @@ def index(request):
 
 
 @csrf_exempt
+def sing_up(request):
+    form = sign_up_form()
+    template = loader.get_template('chess_test/signup.html')
+    if (request.method == 'POST'):
+        print(request.POST.get('username'))
+        formm = sign_up_form(request.POST)
+
+        if (formm.is_valid()):
+            print(x)
+            data = formm.cleaned_data
+            if(User.objects.filter(email = data['email'])):               
+                return render(request, 'chess_test/signup.html', context={'msg': 'You already have profile, sing in'})
+            if(User.objects.filter(username = data['username'])):               
+                return render(request, 'chess_test/signup.html', context={'msg': 'This username is already used'})
+            user = User.objects.create_user(username=data['username'],password=data['password'],email=data['email'])
+            #  user.save()
+            # user = authenticate(request, username=data['username'],password =data['password'])
+            if user is not None:
+              login(request, user)            
+              return redirect('game/')
+            else:
+                print('fuck you')
+    else:
+        formm = sign_up_form()
+    return render(request, 'chess_test/signup.html', context={'msg': ''})
+
+
+@csrf_exempt
 def game(request):
 
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -40,11 +72,11 @@ def game(request):
             })
             return JsonResponse({'pos': {'pawn': ['2:2', '2:3']}})
         elif (request.POST.get('requested') == 'post-fig'):
-           # c.send_data(request.POST.get('move'))
-           pass
+            # c.send_data(request.POST.get('move'))
+            pass
+
     f = {'pawn': [1, 2]}
 
     context = {"figures": f, "a": 7, "b": 6}
     print("Změna")
     return render(request, 'chess_test/chessboard.html', context)
- 
