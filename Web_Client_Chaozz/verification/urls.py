8302fb144862faf0fafa645b3a74677353a3943d
TurekJakub <jakub.turek@student.gyarab.cz>
2023-03-20 22:02:07 +0100

    20/3/23 - verification templates pt. 2

--- Web_Client_Chaozz/verification/urls.py
+++ Web_Client_Chaozz/verification/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("reset/", views.password_reset, name="pasword_reset"),
     path("confirm/", views.confirmation, name="registration_confirmation"),
-    
+    path("reset/<uid>/<token>/", views.set_new_password, name="set_new_password"),
+    path("success", views.success, name="set_new_password")    
 ]
--- Web_Client_Chaozz/verification/views.py
+++ Web_Client_Chaozz/verification/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-def password_reset():
-    pass
+from django.shortcuts import render,HttpResponse
+from django.template import loader
+def password_reset(request):
+     template = loader.get_template('resetpassword.html')
+     return HttpResponse(template.render())
+def set_new_password(request,uid,token):
+     template = loader.get_template('newpassword.html')
+     return HttpResponse(template.render())
 def confirmation():
     pass
+def success(request):
+      return render(request, './successtemplate.html', context={'action': 'Obnovení hesla vašeho účtu'})
 # Create your views here.
