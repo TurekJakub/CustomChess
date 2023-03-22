01537b6372498857cc159120594e89b249349d61
TurekJakub <jakub.turek@student.gyarab.cz>
2023-03-22 23:33:19 +0100

    22/3/23 - web client - authentication

--- Web_Client_Chaozz/chess_test/urls.py
+++ Web_Client_Chaozz/chess_test/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.sign_up, name="index"),
-    path("game/", views.game, name="i"),
-    path("",views.sign_in,name='sing_up'),
-    path("activate/<uid>/<token>/",views.sign_inm,name='sing_up'),
+    path("signup/", views.sign_up, name="sign_up"),
+    path("game/", views.game, name="game"),
+    path("",views.sign_in,name='sign_in'),
 ]
