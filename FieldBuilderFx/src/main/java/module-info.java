aa90867981086fb06c9e0def5add051c60ced80d
DobiasPetr <petr.dobias@student.gyarab.cz>
2023-04-05 08:58:35 +0200

    it is gone

--- FieldBuilderFx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.fieldbuilderfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.fieldbuilderfx to javafx.fxml;
-    exports com.example.fieldbuilderfx;
-}
