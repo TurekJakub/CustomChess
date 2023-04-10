36cc0b9697ee5725676a7b29098aa589417b5767
Foxik-4 <83377656+Foxik-4@users.noreply.github.com>
2023-04-10 19:18:00 +0200

    Update OwOExecutionist.java

--- is-det-sQr-4-Miii/OwOExecutionist.java
+++ is-det-sQr-4-Miii/OwOExecutionist.java
@@ -5,10 +5,10 @@ public class OwOExecutionist {
     public static void main(String[] args) {
         Board board = new Board(8, 8);
 
-        String[] sarr = {"1/2", "om1.(1/3)"};
+        String[] sarr = {"1-2(1/1>)", "om1.(1/3)"};
         Piece p1 = new Piece(sarr);
         p1.mark = "N";
-        board.place(p1, 2, 2);
+        board.place(p1, 4, 4);
 
         Piece p2 = new Piece(sarr);
         p2.mark = "P";
@@ -20,8 +20,10 @@ public class OwOExecutionist {
         MoveInterpreter mi = new MoveInterpreter(null);
         mi.setBoard(board);
 
-        HashSet<int[]> hs = mi.getValidMoves(p1.moves[0], 0, 0, p1);
+        HashSet<int[]> hs = mi.getValidMoves(p1.moves[0], p1.x, p1.y, p1);
         java.util.Iterator<int[]> itr = hs.iterator();
+        board.showMoves(hs);
+        System.out.println("pos: " + p1.x + ", " + p1.y);
         while(itr.hasNext()){
             System.out.print(Arrays.toString(itr.next()));
         }
