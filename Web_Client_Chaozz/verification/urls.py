f838f626f9f327b7a94838e10e781f50ba06a072
TurekJakub <jakub.turek@student.gyarab.cz>
2023-04-12 19:53:46 +0200

    12/4/23 - verification - client side completed

--- Web_Client_Chaozz/verification/urls.py
+++ Web_Client_Chaozz/verification/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path("reset/", views.password_reset, name="pasword_reset"),
-    path("confirm/", views.confirmation, name="registration_confirmation"),
+    path(
+        "confirm/<uid>/<token>/",
+        views.registration_confirmation,
+        name="registration_confirmation",
+    ),
     path("reset/<uid>/<token>/", views.set_new_password, name="set_new_password"),
-    path("success", views.success, name="set_new_password")    
+    path("result/", views.result, name="result"),
 ]
